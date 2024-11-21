39ebe1563f74447fb109d54c89c17e07089ff988
Jayakrishnan K V <jayakrishnankv1994@gmail.com>
2024-11-21 18:34:43 +0100

    test

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 
-# Function to load the dataset
+## Function to load the dataset
 def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
